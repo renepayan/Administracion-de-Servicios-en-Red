880fc0063aecc6622bd2ac75122b0d5d598cd565
René Payán Téllez <payantellez1g@hotmail.com>
2021-12-09 04:27:09 -0600

    Faltan las interfaces de control, los constructores, los getter y setter

--- Proyecto Final/WEB/Clases/GrupoDatos.php
+++ Proyecto Final/WEB/Clases/GrupoDatos.php
@@ -2,7 +2,7 @@
     namespace PBX;
     require_once 'Modelos/Grupo.php';
     class GrupoDatos{
-        public static function getGrupoById($conexion, int $id):?Nodo{
+        public static function getGrupoById($conexion, int $id):?Modelos\Grupo{
             $retorno = null;
             if($pstmSelect = $conexion->prepare("SELECT idGrupo, Extension, Nombre FROM tbl_Grupos WHERE idGrupo = ?")){
                 $pstmSelect->bind_param("i",$id); 
@@ -19,7 +19,7 @@
             }
             return $retorno;
         }
-        public static function getGrupoAleatorio($conexion):?Nodo{
+        public static function getGrupoAleatorio($conexion):?Modelos\Grupo{
             $retorno = null;
             if($pstmSelect = $conexion->prepare("SELECT idGrupo FROM tbl_Grupos ORDER BY RAND() LIMIT 1")){
                 $pstmSelect->execute();
--- Proyecto Final/WEB/Clases/NodoDatos.php
+++ Proyecto Final/WEB/Clases/NodoDatos.php
@@ -2,7 +2,7 @@
     namespace PBX;
     require_once 'Modelos/Nodo.php';
     class NodoDatos{
-        public static function getNodoById($conexion, int $id):?Nodo{
+        public static function getNodoById($conexion, int $id):?Modelos\Nodo{
             $retorno = null;
             if($pstmSelect = $conexion->prepare("SELECT idNodo, Numero, IP, Dominio, Nombre FROM tbl_Nodos WHERE idNodo = ?")){
                 $pstmSelect->bind_param("i",$id); 
@@ -19,7 +19,7 @@
             }
             return $retorno;
         }
-        public static function getNodoAleatorio($conexion):?Nodo{
+        public static function getNodoAleatorio($conexion):?Modelos\Nodo{
             $retorno = null;
             if($pstmSelect = $conexion->prepare("SELECT idNodo FROM tbl_Nodos ORDER BY RAND() LIMIT 1")){
                 $pstmSelect->execute();
