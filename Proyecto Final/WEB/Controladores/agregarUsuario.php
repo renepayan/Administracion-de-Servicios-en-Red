d8142f941a1827f96f5b30580d03da8102e41097
René Payán Téllez <payantellez1g@hotmail.com>
2021-12-09 04:19:54 -0600

    Faltan las interfaces de control, los constructores, los getter y setter

--- Proyecto Final/WEB/Controladores/agregarUsuario.php
+++ Proyecto Final/WEB/Controladores/agregarUsuario.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     require_once 'Clases/Modelos/Usuario.php';
     require_once 'Clases/UsuarioDatos.php';
     require_once 'ajustes.php';
