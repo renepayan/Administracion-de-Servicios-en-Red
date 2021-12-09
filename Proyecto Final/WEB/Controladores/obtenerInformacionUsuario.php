92092d77d82a6683e4c3d8370cc9d9bd6a11325d
René Payán Téllez <payantellez1g@hotmail.com>
2021-12-09 15:59:37 -0600

    Faltan las interfaces de control, los constructores, los getter y setter

--- Proyecto Final/WEB/Controladores/obtenerInformacionUsuario.php
+++ Proyecto Final/WEB/Controladores/obtenerInformacionUsuario.php
@@ -41,7 +41,7 @@
     }
     $conexion->close();
     //ob_clean();    
-    print_r($sal);
+    //print_r($sal);
     header("Content-type: application/json");
     echo(json_encode($sal,JSON_UNESCAPED_UNICODE));
 ?>
