2be790c5427fd03ed5183af6808d1be7d38f2cf8
René Payán Téllez <payantellez1g@hotmail.com>
2021-12-09 14:19:05 -0600

    Faltan las interfaces de control, los constructores, los getter y setter

--- Proyecto Final/WEB/Clases/PermisoLlamadaDatos.php
+++ Proyecto Final/WEB/Clases/PermisoLlamadaDatos.php
@@ -1,8 +1,8 @@
 <?php
     namespace PBX;
-    require_once 'Modelos\PermisoDeLlamada.php';
+    require_once 'Modelos\PermisoLlamada.php';
     class PermisoDeLlamadaDatos{
-        public static function addPermisoDeLlamada($conexion, Modelos\PermisoDeLlamada $permisoDeLlamada):bool{
+        public static function addPermisoDeLlamada($conexion, Modelos\PermisoLlamada $permisoDeLlamada):bool{
             $retorno = false;
             if($pstmInsert = $conexion->prepare("INSERT INTO tbl_PermisosLlamadas (Usuario, NodoOrigen, NodoDestino) VALUES (?,?,?)")){
                 $pstmInsert->bind_param("iii",$permisoDeLlamada->getUsuario()->getId(),$permisoDeLlamada->getNodoOrigen()->getId(),$permisoDeLlamada->getNodoDestino()->getId());
